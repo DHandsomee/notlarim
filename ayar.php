558e936fe68872af639700ce13b0f8551e92eb22
DHandsomee <34570010+DHandsomee@users.noreply.github.com>
2022-08-14 13:41:45 +0200

    Create ayar.php

---
+++ ayar.php
@@ -0,0 +1,18 @@
+# PDO Veritabanı bağlantı ayarları
+
+
+<?php
+
+// Veri tabanı bağlantısı
+
+$host 		= "localhost";
+$dbname 	= "";
+$charset 	= "utf8";
+$root 		= "";
+$password 	= "";
+
+try{
+  $db = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset;", $root, $password);
+}catch(PDOExeption $error){
+  echo $error->getMessage();
+}
